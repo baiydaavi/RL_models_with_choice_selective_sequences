5ce76d719755161dbfae7792d40ec7533d6897ff
Avinash <baidyaavinash@gmail.com>
2021-09-12 14:17:04 -0700

    Update style.

--- run.py
+++ run.py
@@ -10,14 +10,14 @@ num_hidden_units = 128
 gamma = 0.96
 max_episodes = 80001
 max_steps_per_episode = 630
-r_width = 0.3
+reward_width = 0.3
 learning_rate = 0.001
-switch_prob = 0.1
+block_switch_prob = 0.1
 
 choice_selective_activity = np.load('activity.npz')
 
 model_arch = ActorCriticLSTM(num_actions, num_hidden_units, gamma)
-env = TrainingEnvironment(num_states, r_width, switch_prob)
+env = TrainingEnvironment(reward_width, block_switch_prob)
 MetaRLModel = MetaRLModel(model_arch,
                           env,
                           choice_selective_activity,
