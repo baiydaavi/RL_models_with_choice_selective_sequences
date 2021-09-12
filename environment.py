7745374f1634593f408e9c563005c3b0937d678f
Avinash <baidyaavinash@gmail.com>
2021-09-12 13:38:39 -0700

    Add the rest of the reversal learning environments.

--- environment.py
+++ environment.py
@@ -1,5 +1,4 @@
-"""This module defines the reversal learning environments.
-"""
+"""This module defines the reversal learning environments."""
 
 import numpy as np
 
@@ -8,80 +7,371 @@ from utils import normalized_gaussian
 
 class TrainingEnvironment:
     """Training Environment"""
-    def __init__(self, len_t, r_width, switch_prob):
-        self.len_t = len_t
-        self.t_vec = np.round(np.arange(-2.1, 2.1, 0.1), 3)
-        self.r_width = r_width
-        self.switch_prob = switch_prob
-        self.r_time = np.round(np.arange(0.2, 1.3, 0.1), 3)
-        self.time = 0
-        self.timestep = 0
-        self.rewarded = 0
-        self.choice = -1
-        self.bandit = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1, 0.7]
-        self.done = False
 
-        self.r_vec = normalized_gaussian(self.t_vec,
-                                         np.random.choice(self.r_time),
-                                         self.r_width)
-        self.r_vec[self.t_vec <= 0.0] = 0.0
-        self.rand_trial = np.random.randint(100)
-        self.reset()
+    def __init__(self, reward_width, block_switch_prob):
+
+        self.time_array = np.round(np.arange(-2.1, 2.1, 0.1), 3)
+        self.len_time_array = len(self.time_array)
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.mean_reward_time_array = np.round(np.arange(0.2, 1.3, 0.1), 3)
+        self.reward_width = reward_width
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.block_switch_prob = block_switch_prob
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
 
     def reset(self):
-        self.time = 0
-        self.timestep = 0
-        self.rewarded = 0
-        self.choice = -1
-        self.bandit = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1, 0.7]
-        self.done = False
 
-        self.r_vec = normalized_gaussian(self.t_vec, np.random.choice(self.r_time),
-                                   self.r_width)
-        self.r_vec[self.t_vec <= 0.0] = 0.0
-        self.rand_trial = np.random.randint(100)
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
 
     def step(self, action):
 
-        self.timestep += 1
+        self.current_episode_time += 1
 
-        reward = self.r_vec[self.time] * self.rewarded
+        reward = self.reward_array[self.current_trial_time] * self.is_rewarded
 
-        if self.time == 0:
+        # Start of a trial
+        if self.current_trial_time == 0:
+            self.reward_array = normalized_gaussian(self.time_array,
+                                                    np.random.choice(
+                                                        self.mean_reward_time_array),
+                                                    self.reward_width)
+            self.reward_array[self.time_array <= 0.0] = 0.0
+            self.is_rewarded = 0
 
-            self.r_vec = normalized_gaussian(self.t_vec,
-                                       np.random.choice(self.r_time),
-                                       self.r_width)
-            self.r_vec[self.t_vec <= 0.0] = 0.0
-            self.rand_trial = np.random.randint(100)
-            self.rewarded = 0
             self.choice = action - 1
 
-            if np.random.uniform() < self.switch_prob:
-                self.bandit = list(reversed(self.bandit))
+            self.rand_activity_trial_num = np.random.randint(100)
+
+            if np.random.uniform() < self.block_switch_prob:
+                self.reward_prob = list(reversed(self.reward_prob))
 
             if action == 0:
                 reward = -1
 
             else:
-
-                bandit = self.bandit[self.choice]
-
-                result = np.random.uniform()
-
-                if result < bandit:
-                    self.rewarded = 1
+                current_choice_reward_prob = self.reward_prob[self.choice]
+                if np.random.uniform() < current_choice_reward_prob:
+                    self.is_rewarded = 1
 
         else:
-
             if action != 0:
                 reward = -1
 
-        self.time = (self.time + 1) % self.len_t
+        self.current_trial_time = (self.current_trial_time + 1) % \
+                                  self.len_time_array
 
-        return self.time, reward, self.done, self.timestep
+        return self.current_trial_time, reward, self.is_rewarded, \
+               self.current_episode_time
 
 
-# class TestingEnvironment:
+class TestingEnvironment:
+    """Testing Environment"""
 
-# class FixedBlockTestingEnvironment:
+    def __init__(self, reward_width):
+
+        self.time_array = np.round(np.arange(-2.1, 2.1, 0.1), 3)
+        self.len_time_array = len(self.time_array)
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.mean_reward_time_array = np.round(np.arange(0.2, 1.3, 0.1), 3)
+        self.reward_width = reward_width
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+        self.reward_count = 0
+
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
+
+    def reset(self):
+
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+        self.reward_count = 0
+
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
+
+    def step(self, action):
+
+        self.current_episode_time += 1
+
+        reward = self.reward_array[self.current_trial_time] * self.is_rewarded
+
+        # Start of a trial
+        if self.current_trial_time == 0:
+            self.reward_array = normalized_gaussian(self.time_array,
+                                                    np.random.choice(
+                                                        self.mean_reward_time_array),
+                                                    self.reward_width)
+            self.reward_array[self.time_array <= 0.0] = 0.0
+            self.is_rewarded = 0
+
+            self.choice = action - 1
+
+            self.rand_activity_trial_num = np.random.randint(100)
+
+            if self.reward_count > 9:
+                if np.random.uniform() > 0.4:
+                    self.reward_prob = list(reversed(self.reward_prob))
+                    self.reward_count = 0
+
+            if action == 0:
+                reward = -1
+
+            else:
+                current_choice_reward_prob = self.reward_prob[self.choice]
+                if np.random.uniform() < current_choice_reward_prob:
+                    self.is_rewarded = 1
+                    self.reward_count += 1
+
+        else:
+            if action != 0:
+                reward = -1
+
+        self.current_trial_time = (self.current_trial_time + 1) % \
+                                  self.len_time_array
+
+        return self.current_trial_time, reward, self.is_rewarded, \
+               self.current_episode_time
+
+
+class OptoTestingEnvironment:
+    """Testing Environment for the optogenetic activation case."""
+
+    def __init__(self, reward_width):
+
+        self.time_array = np.round(np.arange(-2.1, 2.1, 0.1), 3)
+        self.len_time_array = len(self.time_array)
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.mean_reward_time_array = np.round(np.arange(0.2, 1.3, 0.1), 3)
+        self.reward_width = reward_width
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+        self.reward_count = 0
+
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
+
+        self.is_stimulated = 0
+
+    def reset(self):
+
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+        self.reward_count = 0
+
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
+
+        self.is_stimulated = 0
+
+    def step(self, action):
+
+        self.current_episode_time += 1
+
+        reward = self.reward_array[self.current_trial_time] * self.is_rewarded
+
+        # Start of a trial
+        if self.current_trial_time == 0:
+            self.reward_array = normalized_gaussian(self.time_array,
+                                                    np.random.choice(
+                                                        self.mean_reward_time_array),
+                                                    self.reward_width)
+            self.reward_array[self.time_array <= 0.0] = 0.0
+            self.is_rewarded = 0
+
+            self.choice = action - 1
+
+            self.rand_activity_trial_num = np.random.randint(100)
+
+            if np.random.uniform() < 0.1:
+                self.is_stimulated = 1
+            else:
+                self.is_stimulated = 0
+
+            if self.reward_count > 9:
+                if np.random.uniform() > 0.4:
+                    self.reward_prob = list(reversed(self.reward_prob))
+                    self.reward_count = 0
+
+            if action == 0:
+                reward = -1
+
+            else:
+                current_choice_reward_prob = self.reward_prob[self.choice]
+                if np.random.uniform() < current_choice_reward_prob:
+                    self.is_rewarded = 1
+                    self.reward_count += 1
+
+        else:
+            if action != 0:
+                reward = -1
+
+        if self.current_trial_time == self.len_time_array - 1:
+            self.is_stimulated = 0
+
+        self.current_trial_time = (self.current_trial_time + 1) % \
+                                  self.len_time_array
+
+        return self.current_trial_time, reward, self.is_rewarded, \
+               self.current_episode_time
+
+
+class FixedBlockTestingEnvironment:
+    """Fixed Block Testing Environment"""
+
+    def __init__(self, reward_width):
+
+        self.time_array = np.round(np.arange(-2.1, 2.1, 0.1), 3)
+        self.len_time_array = len(self.time_array)
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.mean_reward_time_array = np.round(np.arange(0.2, 1.3, 0.1), 3)
+        self.reward_width = reward_width
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+        self.trial_count_in_block = 0
+
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
+
+    def reset(self):
+
+        self.current_trial_time = 0
+        self.current_episode_time = 0
+
+        self.reward_array = normalized_gaussian(self.time_array,
+                                                np.random.choice(
+                                                    self.mean_reward_time_array),
+                                                self.reward_width)
+        self.reward_array[self.time_array <= 0.0] = 0.0
+        self.is_rewarded = 0
+        self.trial_count_in_block = 0
+
+        self.reward_prob = [0.7, 0.1] if np.random.uniform() <= 0.5 else [0.1,
+                                                                          0.7]
+
+        self.choice = -1
+
+        self.rand_activity_trial_num = np.random.randint(100)
+
+    def step(self, action):
+
+        self.current_episode_time += 1
+
+        reward = self.reward_array[self.current_trial_time] * self.is_rewarded
+
+        # Start of a trial
+        if self.current_trial_time == 0:
+            self.reward_array = normalized_gaussian(self.time_array,
+                                                    np.random.choice(
+                                                        self.mean_reward_time_array),
+                                                    self.reward_width)
+            self.reward_array[self.time_array <= 0.0] = 0.0
+            self.is_rewarded = 0
+
+            self.choice = action - 1
+
+            self.rand_activity_trial_num = np.random.randint(100)
+
+            if self.trial_count_in_block > 29:
+                self.reward_prob = list(reversed(self.reward_prob))
+                self.trial_count_in_block = 0
+
+            self.trial_count_in_block += 1
+
+            if action == 0:
+                reward = -1
+
+            else:
+                current_choice_reward_prob = self.reward_prob[self.choice]
+                if np.random.uniform() < current_choice_reward_prob:
+                    self.is_rewarded = 1
+
+        else:
+            if action != 0:
+                reward = -1
+
+        self.current_trial_time = (self.current_trial_time + 1) % \
+                                  self.len_time_array
+
+        return self.current_trial_time, reward, self.is_rewarded, \
+               self.trial_count_in_block
